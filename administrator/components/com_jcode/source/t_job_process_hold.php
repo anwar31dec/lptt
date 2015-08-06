d00be9f1fae34c9b3cd4e5bd544c72c26d60491c
Anwar Hossain <anwarcs36@yahoo.com>
2015-08-06 17:26:11 +0600

    Process hold

--- administrator/components/com_jcode/source/t_job_process_hold.php
+++ administrator/components/com_jcode/source/t_job_process_hold.php
@@ -1,9 +1,5 @@
 <?php
 $user = JFactory::getUser();
-
-//print_r($user);
-//exit;
-
 $baseUrl = JURI::base() . 'administrator/components/com_jcode/source/';
 $jBaseUrl = JURI::base();
 $lang = JFactory::getLanguage();
@@ -23,105 +19,116 @@ include_once ('combo_script.php');
 include_once ('language/lang_en.php');
 include_once ('language/lang_fr.php');
 include_once ('language/lang_switcher.php');
+
+$ProcessId = $aUserProcess['ProcessId'];
 ?>
 
 <script type="text/javascript">
     var vLang = '<?php echo $vLang; ?>';
 </script>
 
-<div class="row"> 
-	<div class="col-md-6">
-        <div id="form-panel99">
-	        <div class="panel-heading">
-	            <?php echo $aUserProcess['ProcessName']; ?>           
-	        </div>
-	        <div class="panel-body">
-	            <form novalidate="" data-validate="parsley" id="itemlist_form" class="form-horizontal form-border no-margin">
-										
-	                <div class="form-group" style="<?php if($aUserProcess['bUseTrackingNo']) echo 'display:block;'; else echo 'display:none;';?>">
-	                    <label class="control-label col-md-4" for="TrackingNo"><?php echo 'Inward No:'; ?></label>
-	                    <div class="col-md-8">
-	                        <input class="form-control input-sm parsley-validated" maxlength="100" type="text" name="TrackingNo" id="TrackingNo" data-required="true" placeholder="scan here..."/>
-	                    </div>
-	                </div> 
 
-			<div class="form-group"  style="<?php if($aUserProcess['bUseRegNo']) echo 'display:block;'; else echo 'display:none;';?>">
-	                    <label class="control-label col-md-4" for="RegNo"><?php echo 'Registration No:'; ?></label>
-	                    <div class="col-md-8">
-	                        <input class="form-control input-sm parsley-validated" maxlength="100" type="text" name="RegNo" id="RegNo" data-required="true" placeholder="scan here..."/>
-	                    </div>
-	                </div> 
-	
-	                <div class="form-group">
-	                	<div class="col-md-4">
-					<input type="text" value="insertUpdateProcessTracking" id="action" name="action" style="display: none;"/>
-					<input type="hiidden" style="display:none;"name="ProcessId" id="ProcessId" value="<?php echo $aUserProcess['ProcessId']; ?>"/>
-					<input type="hiidden" style="display:none;"name="ProcessOrder" id="ProcessOrder" value="<?php echo $aUserProcess['ProcessOrder']; ?>"/>
-					<input type="hiidden" style="display:none;"name="ParentProcessId" id="ParentProcessId" value="<?php echo $aUserProcess['ParentProcessId']; ?>"/>
-					<input type="hiidden" style="display:none;"name="eNewNoPosition" id="eNewNoPosition" value="<?php echo $aUserProcess['eNewNoPosition']; ?>"/>
-					<input type="hiidden" style="display:none;"name="bUseRegNo" id="bUseRegNo" value="<?php echo $aUserProcess['bUseRegNo']; ?>"/>
-					<input type="hiidden" style="display:none;"name="Position" id="Position" value="<?php echo $aUserProcess['Position']; ?>"/>
-					<input type="hiidden" style="display:none;"name="hTrackingNo" id="hTrackingNo" />
-					<input type="hiidden" style="display:none;" id="userId" value="<?php echo $user->username; ?>"/>
-					<input type="hiidden" style="display:none;" id="en-GBId" value="<?php echo $lan; ?>"/>
-				</div>
-	                    <div class="col-md-8">		                    	
-		                        <a href="javascript:void(0);" class="btn btn-success btn-form-success" id="submitItemList"><?php echo $TEXT['Submit']; ?></a>
-		                        <a href="javascript:void(0);" class="btn btn-default" onClick="onListPanel()"><?php echo $TEXT['Cancel']; ?></a>			                    
-	                    </div>
-	                </div>
-	            </form>    
-	        </div>      
-	    </div>
-	</div>
-	<div class="col-md-6">		
-		<div id="list-panel">
-            <table  id="itemTable" class="table table-striped table-bordered display table-hover" cellspacing="0">
-                <thead>
-                    <tr>
-						<th></th>
-                        <th style="text-align: center;">SL#</th>
-                        <th><?php echo 'Tracking#'; ?></th>
-                        <th><?php echo 'Process Name'; ?></th>
-                        <th><?php echo 'In Time'; ?></th>
-                        <th><?php echo 'Out Time'; ?></th>
-						<th><?php echo 'Current Duration'; ?></th>
-						<th><?php echo 'Status' ?></th>
-                    </tr>
-                </thead>
-                <tbody></tbody>
-            </table>			                   
-        </div>            
-    </div>  
+<div class="nav-data">
+    <div class="row">       
+        <div class="col-md-6">
+            <h3> <?php echo 'Process: '.$aUserProcess['ProcessName']; ?> </h3>
+        </div>
+        <div class="col-md-6 col-padding">
+            <div class="tbl-header1 pull-right">
+                <label>					
+                    <a data-mce-href="#" class="btn-list but_back" href="javascript:void(0);" onClick="onListPanel()"><i data-mce-bootstrap="1" class="fa fa-reply fa-lg">&nbsp;</i> <?php echo $TEXT['Back to List']; ?></a>
+                    <a style="display: none;" id="PrintBTN" data-mce-href="#" class="but_print" href="javascript:void(0);" onclick="print_function('print')"><i data-mce-bootstrap="1" class="fa fa-print fa-lg">&nbsp;</i> <?php echo $TEXT['Print']; ?></a>
+                    <a style="display:none;" id="PrintBTN1" data-mce-href="#" class="but_excel" href="javascript:void(0);" onclick="print_function('excel')"><i data-mce-bootstrap="1" class="fa fa-file-excel-o fa-lg">&nbsp;</i> <?php echo $TEXT['Excel']; ?></a>
+                </label>
+            </div>
+        </div>
+    </div>
 </div>
 
-<div class="row" style="display:none;"> 
-	
-	<div class="col-md-6">		
-		<div id="list-panel2">
-            <table  id="WaitingProcessList" class="table table-striped table-bordered display table-hover" cellspacing="0">
+
+<div class="row"> 
+    <div class="col-md-12">
+        <div id="list-panel">
+            <table  id="tblProcessTracking" class="table table-striped table-bordered display table-hover" cellspacing="0">
                 <thead>
                     <tr>
-						<th></th>
+                        <th></th>
                         <th style="text-align: center;">SL#</th>
                         <th><?php echo 'Tracking#'; ?></th>
                         <th><?php echo 'Process Name'; ?></th>
                         <th><?php echo 'In Time'; ?></th>
                         <th><?php echo 'Out Time'; ?></th>
-						<th><?php echo 'Current Duration'; ?></th>
-						<th><?php echo 'Status' ?></th>
+                        <th><?php echo 'Current Duration'; ?></th>
+                        <th><?php echo 'Status' ?></th>
+                        <th><?php echo 'bHold'; ?></th>
+                        <th><?php echo 'Hold'; ?></th>
+                        <th><?php echo 'Comments'; ?></th>
+                        <th style="text-align: center;">Action</th>
+                        <th><?php echo 'ProcessId'; ?></th>                        
                     </tr>
                 </thead>
                 <tbody></tbody>
-            </table>			                   
-        </div>            
-    </div>  
+            </table>        			                   
+        </div>
+
+
+        <div id="form-panel">
+            <div class="panel-heading">
+                Job Hold Entry          
+            </div>
+            <div class="panel-body">
+                <form novalidate="" data-validate="parsley" id="process_hold_form" class="form-horizontal form-border no-margin">
+
+                    <div class="form-group">
+                        <label class="control-label col-md-4" for="TrackingNo">Tracking No:</label>
+                        <div class="col-md-8">
+                            <input class="form-control input-sm parsley-validated" maxlength="100" type="text" name="TrackingNo" id="TrackingNo" data-required="true" placeholder="input here..." disabled="true"/>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="control-label col-md-4" for="HoldComments">Comments*:</label>
+                        <div class="col-md-8">
+                            <textarea class="form-control input-sm parsley-validated" rows="5" name="HoldComments" id="HoldComments" data-required="true" placeholder="Your message"></textarea>                           
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">		
+                            <div class="form-group">
+                                <label class="control-label col-md-6" for="bHold">Hold/Unhold</label>
+                                <div class="col-md-6">
+                                    <input type="checkbox" name="bHold" id="bHold"/>
+                                    <span class="custom-checkbox"></span> 
+                                </div>
+
+                            </div>
+                        </div>                        
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-md-4">
+                            <input type="text" value="insertUpdateProcessTracking" id="action" name="action" style="display: none;"/>
+                            <input type="hiidden" style="display:none;" name="ProcessId" id="ProcessId" value="<?php echo $aUserProcess['ProcessId']; ?>"/>
+                            <input type="hiidden" style="display:none;" name="hTrackingNo" id="hTrackingNo"/>
+                            <input type="hiidden" style="display:none;" id="userId" value="<?php echo $user->username; ?>"/>
+                            <input type="hiidden" style="display:none;" id="en-GBId" value="<?php echo $lan; ?>"/>
+                        </div>
+                        <div class="col-md-8">		                    	
+                            <a href="javascript:void(0);" class="btn btn-success btn-form-success" id="submitItemList"><?php echo $TEXT['Submit']; ?></a>
+                            <a href="javascript:void(0);" class="btn btn-default" onClick="onListPanel()"><?php echo $TEXT['Cancel']; ?></a>			                    
+                        </div>
+                    </div>
+                </form>    
+            </div>      
+        </div>
+    </div>
 </div>
 
 
 
 <script>
-    String.prototype.replaceAll = function(token, newToken, ignoreCase) {
+    String.prototype.replaceAll = function (token, newToken, ignoreCase) {
         var _token;
         var str = this + "";
         var i = -1;
@@ -164,7 +171,7 @@ include_once ('language/lang_switcher.php');
         var colorCodeIndex = Array();
         var checkBoxIndex = new Array();
         var columns = $('#' + dataTableId).dataTable().dataTableSettings[0].aoColumns;
-        $.each(columns, function(i, v) {
+        $.each(columns, function (i, v) {
             if (v.bVisible) {
                 tableHeaderList.push(v.sTitle);
                 tableHeaderWidth.push(v.sWidth);
@@ -175,7 +182,7 @@ include_once ('language/lang_switcher.php');
         var reportHeaderName = TEXT['Products List'];
         reportHeaderList[0] = reportHeaderName;
         //reportHeaderList[1] = TEXT['Product Group'] + ': ' + $('#item-group option[value=' + $('#item-group').val() + ']').text();
-        reportHeaderList[1] = TEXT['Product Group'] + ': ' +$('#item-group').find(":selected").text();
+        reportHeaderList[1] = TEXT['Product Group'] + ': ' + $('#item-group').find(":selected").text();
 
         sqlParameterList[0] = (($('#item-group').val() == '') ? 0 : $('#item-group').val());
         groupBySqlIndex = 7;
@@ -257,7 +264,7 @@ include_once ('language/lang_switcher.php');
                     colorCodeIndex: colorCodeIndex,
                     checkBoxIndex: checkBoxIndex
                 },
-                success: function(response) {
+                success: function (response) {
                     if (response == 'Processing Error') {
                         alert('No Record Found.');
                     } else {
@@ -269,15 +276,16 @@ include_once ('language/lang_switcher.php');
         }
 
     }
-	
-	
 
 </script>
 
 <style>
-	#itemlist_form select, #itemlist_form input{
-		max-width: 300px;
-	}
+    #process_hold_form select, #process_hold_form input{
+        max-width: 300px;
+    }
+    #ConversionFactor{
+        text-align: right !important;
+    }
 </style>
 
 <link href="<?php echo $baseUrl; ?>media/datatable-bootstrap/dataTables.bootstrap.css" rel="stylesheet"/>
@@ -285,7 +293,7 @@ include_once ('language/lang_switcher.php');
 <script src='<?php echo $baseUrl; ?>media/datatable/js/jquery.dataTables.min.js'></script>
 <script src='<?php echo $baseUrl; ?>media/datatable-bootstrap/dataTables.bootstrap.min.js'></script>
 
-<script src='<?php echo $baseUrl; ?>t_itemlist.js'></script>
+<script src='<?php echo $baseUrl; ?>t_job_process_hold.js'></script>
 
 <script type="text/javascript" src="<?php echo $baseUrl; ?>language/lang_en.js"></script>
 <script type="text/javascript" src="<?php echo $baseUrl; ?>language/lang_fr.js"></script>
